5eeabe15ac775ea7851aabe4732036e7712a3ff9
RobertoRamos <robram91@gmail.com>
2020-08-03 15:23:18 -0400

    first commit, added docs

---
+++ doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "bakery-api",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-08-03T19:19:07.465Z",
+    "url": "http://apidocjs.com",
+    "version": "0.24.0"
+  }
+});
