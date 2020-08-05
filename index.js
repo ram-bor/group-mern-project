ef80a682b200740fa267b3e06dc751ac010b2e2a
RobertoRamos <robram91@gmail.com>
2020-08-05 12:00:48 -0400

    index fixes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const app = require("express")();
-const Item = require("./models/Item");
-const Order = require("./models/Order");
+const Item = require("./lib/models/Item");
+const Order = require("./lib/models/Order");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
 // create routes
-app.use(require("./routes/route-index"));
-app.use(require("./routes/order-route"));
-app.use(require("./routes/item-route"));
+app.use(require("./lib/routes/route-index"));
+app.use(require("./lib/routes/order-route"));
+app.use(require("./lib/routes/item-route"));
 
 // set up Heroku's production port + local development port
 app.set("port", process.env.PORT || 8000);
